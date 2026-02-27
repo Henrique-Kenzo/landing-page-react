f0a2f007aa8b1bd1999e76f4e0d7d2a67da3e2b3
Henrique-Kenzo <henrique.silvatte2@gmail.com>
2026-02-27 10:15:33 -0300

    refactor: melhora estrutura da API com env, CORS, tipagem e tratamento de erros

--- landing-page-backend/src/server.ts
+++ landing-page-backend/src/server.ts
@@ -1,26 +1,46 @@
-import express from 'express';
+import express, { type Request, type Response } from 'express';
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST']
+}));
 
 app.use(express.json());
 
-app.get('/', (req, res)=>{
+interface Lead{
+    nome: string;
+    email: string;
+    tel: string;
+    cargo: string;
+}
+
+app.get('/', (req: Request, res: Response)=> {
     res.send('Backend da Landing Page rodando perfeitamente! 🚀')    
     });
 
-app.post('/api/leads', (req, res) => {
-    const { nome, tel, email, cargo } = req.body;
+app.post('/api/leads', (req: Request<{}, {}, Lead>, res: Response)=> {
+
+    try {
+        const { nome, tel, email, cargo } = req.body;
 
     if(!email || !nome || !cargo || !tel){
-        return res.status(400).json({ error: 'E-mail, nome, cargo e telefone são obrigatórios!'});
+        return res.status(400).json({ error: 'Dados inválidos ou Faltando dados!'});
     }
 
-    console.log('Novo lead Capturado:', email, nome, cargo, tel);
     res.status(201).json({ message: 'Lead cadastrado na lista de espera!'});
+
+} catch (error) {
+    console.error('Erro ao cadastrar lead:', error);
+    res.status(500).json({ error: 'Erro interno no servidor!'});
+}
+
 });
 
 app.listen(PORT, () => {
